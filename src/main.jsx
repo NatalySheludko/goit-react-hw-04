b8e849d4d759b434e9b21b549ac04a4b78fdff8e
nataliia <sheludkonataliyavaleriivna@gmail.com>
2024-06-21 20:19:59 +0300

    change name index.css file

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
-import "./index.module.css";
+import "./index.css";
 import "modern-normalize";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
